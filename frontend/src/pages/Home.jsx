9122b5376fe414dc95b40897e7128cc7e57f95da
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-11-12 11:44:37 +0530

    create home page and import navbar and footer

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <div style={{ height: '100vh' }}>
+
+            </div>
+            <Footer />
+        </>
+    )
+}
