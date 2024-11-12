4475a0b902bd41eef14cda1fd27970e1f1d08f6b
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-11-12 11:45:44 +0530

    implement Routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/Home';
+import NotFound from './pages/NotFoundPage'
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   )
 }
 
