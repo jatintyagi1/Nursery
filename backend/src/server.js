b9d61bd2ec1357f15d625c9cd770d4102dc01f4c
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-11-13 11:51:53 +0530

    setup database connection in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
+// server.js
 const express = require('express');
+const connectDB = require('./config/database');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
 
 // Define a simple route
 app.get('/', (req, res) => {
