077de25b80634f193d7d4da20e265ab3b4d272ff
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-11-14 11:47:08 +0530

    add a loading spinner

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { Suspense } from 'react'
+import Loading from './components/Loading/Loading.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<Loading />}>
+      <App />
+    </Suspense>
   </StrictMode>,
 )
